c13e64843884e4c6e191fcc1c03021e718f269aa
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-06 00:53:59 -0700

    改引用

--- 插件.js
+++ 插件.js
@@ -13,7 +13,7 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('恭喜,插件已激活');
-	require('./补全')(context);
+	require('./补全实现')(context);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
